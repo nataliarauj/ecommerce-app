111c5fda582534af67813774fd42027ae8d284e2
nataliarauj <fca.nataliaraujo@gmail.com>
2025-07-09 12:45:19 -0300

    feat: add sign up route

---
+++ src/controllers/auth.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { prismaClient } from "..";
+import {hashSync} from 'bcrypt';
+
+export const signup = async (req: Request, res: Response) => {
+    const {email, password, name} = req.body;
+
+    let user = await prismaClient.user.findFirst({where: {email}});
+
+    if (user) {
+        res.status(409).json({
+            message: 'User already exists!'
+        });
+    } 
+
+    user = await prismaClient.user.create({
+        data: {
+            name,
+            email,
+            password: hashSync(password, 10)
+        }
+    });
+
+    res.json(user);
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from 'express';
-const app = express();
+import { PORT } from '../secrets';
+import rootRouter from './routes';
+import {PrismaClient } from '@prisma/client';
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('OK')
+const app: Express = express();
+
+app.use(express.json());
+app.use('/api', rootRouter);
+
+
+export const prismaClient = new PrismaClient({
+    log: ['query'],
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log('Server online');
 });

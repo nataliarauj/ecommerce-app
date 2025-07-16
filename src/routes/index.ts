0b966d131f8bd51da3c458b66c763b98cab90d9b
nataliarauj <fca.nataliaraujo@gmail.com>
2025-07-16 08:53:52 -0300

    feat: add user address routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import authRoutes from '../routes/auth';
 import productsRoutes from './products';
+import usersRoutes from './users';
 const rootRouter: Router = Router();
 
+rootRouter.use('/users', usersRoutes);
 rootRouter.use('/auth', authRoutes);
 rootRouter.use('/products', productsRoutes);
+
 
 export default rootRouter;
---
+++ src/routes/users.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import authMiddleware from '../middlewares/auth';
+import { errorHandler } from '../error-handler';
+import { addAddress, deleteAddress, listAddress, updateUser } from '../controllers/user';
+import adminMiddleware from '../middlewares/admin';
+
+const usersRoutes: Router = Router();
+
+usersRoutes.post('/address', [authMiddleware, adminMiddleware], errorHandler(addAddress));
+usersRoutes.delete('/address/:id', [authMiddleware], errorHandler(deleteAddress));
+usersRoutes.get('/address', [authMiddleware], listAddress);
+usersRoutes.patch('/', [authMiddleware], errorHandler(updateUser));
+
+export default usersRoutes;

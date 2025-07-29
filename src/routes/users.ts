646a7b2a84073c607a04ab8c81195a351e4b05a1
Natália Araújo <fca.nataliaraujo@gmail.com>
2025-07-29 13:44:44 -0300

    docs: add swaggerUi docs for users Co-authored-by: nataliarauj <fca.nataliaraujo@gmail.com>

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -11,17 +11,66 @@ import adminMiddleware from '../middlewares/admin';
 
 const usersRoutes: Router = Router();
 
+/**
+ * @openapi
+ * /address:
+ *   post: 
+ *     summary: adicionar um endereço relacionado ao usuário
+ *     tags:
+ *         - Users 
+ *     responses: 
+ *       200:
+ *         description: [Descrição do retorno]
+ */
+
 usersRoutes.post(
     '/address',
     [authMiddleware, adminMiddleware],
     errorHandler(addAddress)
 );
+
+/**
+ * @openapi
+ * /address/:id:
+ *   delete: 
+ *     summary: deletar um endereço de acordo com o Id do usuário
+ *     tags:
+ *         - Users 
+ *     responses: 
+ *       200:
+ *         description: [Descrição do retorno]
+ */
+
 usersRoutes.delete(
     '/address/:id',
     [authMiddleware],
     errorHandler(deleteAddress)
 );
+
+/**
+ * @openapi
+ * /address:
+ *   get: 
+ *     summary: listar endereços cadastrados
+ *     tags:
+ *        - Users 
+ *     responses: 
+ *       200:
+ *         description: Retorna um endereço
+ */
 usersRoutes.get('/address', [authMiddleware], listAddress);
+
+/**
+ * @openapi
+ * /:
+ *   patch: 
+ *     summary: alterar um campo de endereço do usuário
+ *     tags:
+ *        - Users 
+ *     responses: 
+ *       200:
+ *         description: Pode alterar qualquer campo 
+ */
 usersRoutes.patch('/', [authMiddleware], errorHandler(updateUser));
 
 export default usersRoutes;

28b4a5081d735d3e22c4c26d2038f976b102f8fa
nataliarauj <fca.nataliaraujo@gmail.com>
2025-08-06 11:38:57 -0300

    docs: add docs from routes of products table

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,43 +1,106 @@
 import { Router } from 'express';
 import { errorHandler } from '../error-handler';
 import {
-    createProduct,
-    deleteProduct,
-    getProductById,
-    listProducts,
-    searchProducts,
-    updateProduct,
+	createProduct,
+	deleteProduct,
+	getProductById,
+	listProducts,
+	searchProducts,
+	updateProduct,
 } from '../controllers/products';
 import authMiddleware from '../middlewares/auth';
 import adminMiddleware from '../middlewares/admin';
 
 const productsRoutes: Router = Router();
+
+/**
+ * @openapi
+ * /search?q="":
+ *   get:
+ *     summary: pesquisar por produto por query
+ *     tags:
+ *         - Products
+ *     responses:
+ *       200:
+ *         description: [Descrição do retorno]
+ */
 productsRoutes.get('/search', errorHandler(searchProducts));
 
-productsRoutes.post(
-    '/',
-    [authMiddleware, adminMiddleware],
-    errorHandler(createProduct)
-);
+/**
+ * @openapi
+ * /:
+ *   post:
+ *     summary: criar um novo produto
+ *     tags:
+ *         - Products
+ *     responses:
+ *       200:
+ *         description: [Descrição do retorno]
+ */
+productsRoutes.post('/', [authMiddleware, adminMiddleware], errorHandler(createProduct));
 
+/**
+ * @openapi
+ * /id:
+ *   patch:
+ *     summary: alterar campos da tabela produtos
+ *     tags:
+ *         - Products
+ *     responses:
+ *       200:
+ *         description: [Descrição do retorno]
+ */
 productsRoutes.patch(
-    '/:id',
-    [authMiddleware, adminMiddleware],
-    errorHandler(updateProduct)
+	'/:id',
+	[authMiddleware, adminMiddleware],
+	errorHandler(updateProduct)
 );
 
+/**
+ * @openapi
+ * /id:
+ *   delete:
+ *     summary: deletar produto por id
+ *     tags:
+ *         - Products
+ *     responses:
+ *       200:
+ *         description: [Descrição do retorno]
+ */
 productsRoutes.delete(
-    '/:id',
-    [authMiddleware, adminMiddleware],
-    errorHandler(deleteProduct)
+	'/:id',
+	[authMiddleware, adminMiddleware],
+	errorHandler(deleteProduct)
 );
 
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     summary: listar todos os produtos cadastrados
+ *     tags:
+ *         - Products
+ *     responses:
+ *       200:
+ *         description: [Descrição do retorno]
+ */
 productsRoutes.get('/', errorHandler(listProducts));
 
+/**
+ * @openapi
+ * /id:
+ *   get:
+ *     summary: pesquisar produto por id
+ *     tags:
+ *         - Products
+ *     responses:
+ *       200:
+ *         description: [Descrição do retorno]
+ */
 productsRoutes.get(
-    '/:id',
-    [authMiddleware, adminMiddleware],
-    errorHandler(getProductById)
+	'/:id',
+	[authMiddleware, adminMiddleware],
+	errorHandler(getProductById)
 );
 
 export default productsRoutes;
